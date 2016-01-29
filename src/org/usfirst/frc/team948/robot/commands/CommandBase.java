a3b9adaa4fd2e3805fbd8e4665852767daeeeed4
Aviral2001 <meaviralgupta@gmail.com>
2016-01-28 20:14:28 -0800

    Minor Update to Command Base
    I just changed the position of one thing to make it more organized.

--- src/org/usfirst/frc/team948/robot/commands/CommandBase.java
+++ src/org/usfirst/frc/team948/robot/commands/CommandBase.java
@@ -9,9 +9,10 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public abstract class CommandBase extends Command {
 	public static Drive drive = new Drive();
+	public static Shooter shooter = new Shooter();
 	
 	public static DS2016 ds = new DS2016();
 	
 	public static Preferences preferences = Preferences.getInstance();
-	public static Shooter shooter = new Shooter();
+	
 }
