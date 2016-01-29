b83a28e2b2ab8fda24bccaf4886ce330e3fc5a42
Knayam7 <mayank.motukuri@gmail.com>
2016-01-28 20:10:50 -0800

    ManualDriveStraight
    Rewrote the ManualDriveStraight command using driveOnHeadingInit and
    driveOnHeading from the Drive subsystem.

--- src/org/usfirst/frc/team948/robot/commands/ManualDriveStraight.java
+++ src/org/usfirst/frc/team948/robot/commands/ManualDriveStraight.java
@@ -5,29 +5,34 @@ import org.usfirst.frc.team948.robot.subsystems.Drive;
 
 public class ManualDriveStraight extends CommandBase {
 	private Drive drive = CommandBase.drive;
-
+	private double heading;
 	public ManualDriveStraight() {
 		requires(drive);
 	}
 
 	protected void initialize() {
+		heading = drive.driveOnHeadingInit(1.0);
 	}
 
 	// Drives on a fixed heading using the right joystick y-value for power
 	protected void execute() {
-		drive.rawTankDrive(DS2016.leftJoystick.getY(), DS2016.leftJoystick.getY());
+		drive.driveOnHeading(heading, ds.getRightJSY());
 	}
 
 	// Command runs forever, until it is interrupted or the battery goes dead
 	protected boolean isFinished() {
 		return false;
 	}
-
+   //Add a comment here later
 	protected void end() {
 		drive.rawStop();
+		drive.driveOnHeadingEnd();
+		drive.setDesiredHeading(heading);
+		
 	}
 
 	protected void interrupted() {
 		end();
+		
 	}
 }
