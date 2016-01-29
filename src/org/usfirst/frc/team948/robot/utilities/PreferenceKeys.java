2f61918f5be10489b9197fde5ce713308a2da388
sonyalao <lao.sonya@gmail.com>
2016-01-28 19:55:47 -0800

    Added PreferenceKeys class
    Rachel did this

---
+++ src/org/usfirst/frc/team948/robot/utilities/PreferenceKeys.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc.team948.robot.utilities;
+
+public class PreferenceKeys {
+	String Drive_Straight_On_Heading_P = "DriveStriaghtP";
+	String Drive_Straight_On_Heading_I = "DriveStriaghtI";
+	String Drive_Straight_On_Heading_D = "DriveStriaghtD";
+
+}
