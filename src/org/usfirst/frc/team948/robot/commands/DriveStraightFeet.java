d8851afc798afd8632cc4c43348ff938bfb1bd3b
Sai Bulusu <sai.bulusu@gmail.com>
2016-01-28 19:05:03 -0800

    Cleaned up code

--- src/org/usfirst/frc/team948/robot/commands/DriveStraightFeet.java
+++ src/org/usfirst/frc/team948/robot/commands/DriveStraightFeet.java
@@ -4,8 +4,7 @@ import org.usfirst.frc.team948.robot.RobotMap;
 import org.usfirst.frc.team948.robot.utilities.MathHelper;
 import org.usfirst.frc.team948.robot.utilities.PositionTracker;
 
-public class DriveStraightFeet extends CommandBase {
-
+public class DriveStraightFeet extends CommandBase{
 	public double feet;
 	public double power;
 	public double startN, startE;
@@ -22,15 +21,17 @@ public class DriveStraightFeet extends CommandBase {
 	@Override
 	protected void execute() {
 		// TODO Auto-generated method stub
-		// We take the absolute values, since we just need the total distance traveled if the robot is going backwards
-				double currentN = PositionTracker.currentPN;
-				double currentE = PositionTracker.currentPE;
-				distanceSoFar = Math.sqrt((currentN - startN) * (currentN - startN) + (currentE - startE) * (currentE - startE));
-				double feetRemaining = feet - distanceSoFar;
-				// This ramps the power down to zero when we reach the target distance
-				double powerMod = MathHelper.clamp(power * feetRemaining / SLOWING_DOWN_DISTANCE,
-						-Math.abs(power), Math.abs(power));
-				drive.rawTankDrive(powerMod, powerMod);
+		// We take the absolute values, since we just need the total distance
+		// traveled if the robot is going backwards
+		double currentN = PositionTracker.currentPN;
+		double currentE = PositionTracker.currentPE;
+		distanceSoFar = Math.sqrt((currentN - startN) * (currentN - startN)
+				+ (currentE - startE) * (currentE - startE));
+		double feetRemaining = feet - distanceSoFar;
+		// This ramps the power down to zero when we reach the target distance
+		double powerMod = MathHelper.clamp(power * feetRemaining
+				/ SLOWING_DOWN_DISTANCE, -Math.abs(power), Math.abs(power));
+		drive.rawTankDrive(powerMod, powerMod);
 	}
 
 	@Override
