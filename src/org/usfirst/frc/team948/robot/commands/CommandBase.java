6433a50a0e128db9c25c86c47c8875a017cb69da
ValliappanValliappan <svalliap@gmail.com>
2016-01-31 00:36:42 +0530

    Created an Acquirer object in CommandBase.
    Created an Acquirer object in CommandBase.

--- src/org/usfirst/frc/team948/robot/commands/CommandBase.java
+++ src/org/usfirst/frc/team948/robot/commands/CommandBase.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team948.robot.commands;
 
 import org.usfirst.frc.team948.robot.DS2016;
+import org.usfirst.frc.team948.robot.subsystems.Acquirer;
 import org.usfirst.frc.team948.robot.subsystems.Drive;
 import org.usfirst.frc.team948.robot.subsystems.Shooter;
 
@@ -10,9 +11,9 @@ import edu.wpi.first.wpilibj.command.Command;
 public abstract class CommandBase extends Command {
 	public static Drive drive = new Drive();
 	public static Shooter shooter = new Shooter();
-	
+	public static Acquirer acquirer = new Acquirer();
 	public static DS2016 ds = new DS2016();
-	
+
 	public static Preferences preferences = Preferences.getInstance();
-	
+
 }
