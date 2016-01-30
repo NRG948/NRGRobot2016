c3b9111dfd4b34a58c071eebd2146ba5837973ea
Jasonlee05077 <jason1998.jl@gmail.com>
2016-01-30 13:16:54 -0800

    Added ResetSensors command
    Created a constructor. Made the command initialize nothing, execute the
    resets, and finish when the gyro is 0.5 degree from 0.

---
+++ src/org/usfirst/frc/team948/robot/commands/ResetSensors.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team948.robot.commands;
+
+import org.usfirst.frc.team948.robot.RobotMap;
+
+public class ResetSensors extends CommandBase {
+	
+	public ResetSensors(){
+		
+	}
+	protected void initialize(){
+	
+	}
+	protected void execute(){
+		RobotMap.ahrs.reset();
+		RobotMap.driveGyro.reset();
+		RobotMap.leftMotorEncoder.reset();
+		RobotMap.leftShooterWheelEncoder.reset();
+		RobotMap.rightMotorEncoder.reset();
+		RobotMap.rightShooterWheelEncoder.reset();
+	    
+	}
+	protected boolean isFinished(){
+		return Math.abs(RobotMap.driveGyro.getAngle()) < 0.5;
+		
+	}
+	protected void end(){
+		
+	}
+	
+	protected void interrupted(){
+		
+	}
+
+}
