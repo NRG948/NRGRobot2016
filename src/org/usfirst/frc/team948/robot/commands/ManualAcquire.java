6c681f3a5e6232d5eb3e4ac2045ed9ed7d685497
j-ouyang <jamesouyang00@gmail.com>
2016-02-02 20:00:23 -0800

    added manual acquire command

---
+++ src/org/usfirst/frc/team948/robot/commands/ManualAcquire.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team948.robot.commands;
+
+
+
+/**
+ *
+ */
+public class ManualAcquire extends CommandBase {
+double power;
+    public ManualAcquire(double power) {
+    	this.power = power;
+    	requires(acquirer);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	acquirer.
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
