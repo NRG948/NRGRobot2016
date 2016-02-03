5b092bda08133a4d4ea72c869fc44c97a32027c2
Sai Bulusu <sai.bulusu@gmail.com>
2016-02-02 19:42:12 -0800

    Added Interrupt Command
    Interrupt command in commands package #60

---
+++ src/org/usfirst/frc/team948/robot/commands/Interrupt.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team948.robot.commands;
+
+public class Interrupt extends CommandBase{
+
+	public Interrupt() {
+		requires(drive);
+		requires(acquirer);
+		requires(shooter);
+	}
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+}
