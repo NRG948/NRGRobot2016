3cc6c05d03ab47d45010c46bfc92f7c9443558ff
Jasonlee05077 <jason1998.jl@gmail.com>
2016-02-02 20:36:36 -0800

    Partial Position 3D system
    Lots of math

--- src/org/usfirst/frc/team948/robot/utilities/PositionTracker3D.java
+++ src/org/usfirst/frc/team948/robot/utilities/PositionTracker3D.java
@@ -7,7 +7,11 @@ import com.kauailabs.navx.frc.AHRS;
 public class PositionTracker3D {
 
 	private static AHRS ahrs = RobotMap.ahrs;
-	
+	private static double[] i = { 1, 0, 0 };
+	private static double[] j = { 0, 1, 0 };
+	private static double[] k = { 0, 0, 1 };
+	public static double dt = 0.02; // 20 milliseconds check later
+
 	// North(Y), East(X), Up(Z) CHECK LATER WHEN TESTING
 	// Position coordinates in the fixed frame
 	public static double currentPN, currentPE, currentPU;
@@ -24,9 +28,39 @@ public class PositionTracker3D {
 		currentVE = ahrs.getVelocityX();
 		currentVN = ahrs.getVelocityY();
 		currentVU = ahrs.getVelocityZ();
-	
+
 		currentPE = ahrs.getDisplacementX();
 		currentPN = ahrs.getDisplacementY();
 		currentPU = ahrs.getDisplacementZ();
+	}
+
+	public static void computePosition() {
+		// unit vector of the rotation
+		double ox = ahrs.getRawGyroX() * Math.PI / 180;
+		double oy = ahrs.getRawGyroY() * Math.PI / 180;
+		double oz = ahrs.getRawGyroZ() * Math.PI / 180;
+
+		double omega = Math.sqrt(ox * ox + oy * oy + oz * oz);
+		double ux = ox / omega;
+		double uy = oy / omega;
+		double uz = oz / omega;
+		// Angle of the rotation
+		double angle = omega * dt;
+		// Rotation matrix
+		double cos = Math.cos(angle);
+		double sin = Math.sin(angle);
+		double[][] rot = {
+				{ cos + ux * ux * (1 - cos), ux * uy * (1 - cos) - uz * sin, ux * uz * (1 - cos) + uy * sin },
+				{ ux * uy * (1 - cos) + uz * sin, cos + uy * uy * (1 - cos), uy * uz * (1 - cos) - ux * sin },
+				{ ux * uz * (1 - cos) - uy * sin, uy * uz * (1 - cos) + ux * sin, cos + uz * uz * (1 - cos) }};
+		double [] inew = new double [3];
+		double [] jnew = new double [3];
+		double [] knew = new double [3];
+		
+		for (int index = 0;index<3;index++){
+			inew[index] = rot[0][0]*i[index]+rot[1][0]*j[index]+rot[2][0]*k[index];
+			jnew[index] = rot[0][1]*i[index]+rot[1][1]*j[index]+rot[2][1]*k[index];
+			knew[index] = rot[0][2]*i[index]+rot[1][2]*j[index]+rot[2][2]*k[index];
+		}
 	}
 }
