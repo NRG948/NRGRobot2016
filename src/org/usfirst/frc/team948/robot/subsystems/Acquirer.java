4bb612ea9cf75f87c1fadc1adddc8770f470c654
Gregory Quick <gregory@quicks.me>
2016-02-04 18:55:43 -0800

    #66 Changed rawAcquire to take the double speed as input.
    Also removed the constant ACQUIRER_SPEED

--- src/org/usfirst/frc/team948/robot/subsystems/Acquirer.java
+++ src/org/usfirst/frc/team948/robot/subsystems/Acquirer.java
@@ -14,7 +14,6 @@ public class Acquirer extends Subsystem implements PIDOutput {
 	private double pidOutput;
 	private final double ANGLE_TO_VOLTS = 0.01389;
 	private final double TOLERANCE = 1.0 * ANGLE_TO_VOLTS;
-    private final double ACQUIRER_SPEED = 0.5;
 
 
 public Acquirer() {
@@ -52,8 +51,8 @@ public Acquirer() {
 		RobotMap.acquireWheelVictor.disable();;
 	}
 	
-	public void rawAcquire() {
-		RobotMap.acquireWheelVictor.set(ACQUIRER_SPEED);
+	public void rawAcquire(double speed) {
+		RobotMap.acquireWheelVictor.set(speed);
 	}
 	
 	@Override
