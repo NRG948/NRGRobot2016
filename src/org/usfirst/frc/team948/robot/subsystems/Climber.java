fb8b641f7bb60b42310ef67480aa28ff704affb9
ValliappanValliappan <svalliap@gmail.com>
2016-02-06 23:49:47 +0530

    #85
    Deleted the methods in Climber and replaced them with a method
    runClimber.
    It takes in a parameter power and set that equal to BOTH the winch and
    the tapemeasure.

--- src/org/usfirst/frc/team948/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team948/robot/subsystems/Climber.java
@@ -4,18 +4,15 @@ import org.usfirst.frc.team948.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Climber extends Subsystem{
+public class Climber extends Subsystem {
 
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
-		}
-	public void windWinch(double power){
+	}
+
+	public void runClimber(double power) {
+		RobotMap.climberTapeMeasure.set(power);
 		RobotMap.climberWinch.set(power);
 	}
-
-	public void extendTapeMeasure(double power){
-		RobotMap.climberTapeMeasure.set(power);;
-	}
-
 }
