1f49ed2db1c809869246c8b0be71e07468ef5fc2
ValliappanValliappan <svalliap@gmail.com>
2016-02-06 23:36:40 +0530

    #82
    Created a new constructor that takes in a Power for ShooterRampUp. The
    default power is 1.0.

--- src/org/usfirst/frc/team948/robot/commands/ShooterRampUp.java
+++ src/org/usfirst/frc/team948/robot/commands/ShooterRampUp.java
@@ -2,25 +2,38 @@ package org.usfirst.frc.team948.robot.commands;
 
 import org.usfirst.frc.team948.robot.RobotMap;
 
-public class ShooterRampUp extends CommandBase{
-	
-	public ShooterRampUp(){
+public class ShooterRampUp extends CommandBase {
+	private double power;
+	private final double DEFAULT_POWER = 1;
+
+	public ShooterRampUp() {
 		requires(shooter);
+		this.power = DEFAULT_POWER;
 	}
+
+	public ShooterRampUp(double power) {
+		requires(shooter);
+		this.power = power;
+	}
+
 	protected void initialize() {
-	
+
 	}
+
 	protected void execute() {
-		RobotMap.leftShooterWheel.set(1.0);	
-		RobotMap.rightShooterWheel.set(1.0);
+		RobotMap.leftShooterWheel.set(-power);
+		RobotMap.rightShooterWheel.set(power);
 	}
+
 	protected boolean isFinished() {
 		return false;
 	}
+
 	protected void end() {
 		RobotMap.leftShooterWheel.set(0);
 		RobotMap.rightShooterWheel.set(0);
 	}
+
 	@Override
 	protected void interrupted() {
 		// TODO Auto-generated method stub
