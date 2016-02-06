ea4b36c7b1d5e72ec438662814f30eea4c6b6044
Jasonlee05077 <jason1998.jl@gmail.com>
2016-02-06 15:52:37 -0800

    added calcDistance() method with member variables
    also changed the name of tableTwo to tableTotal Area. Added the member
    variables of TARGET_FEET, FOV_ANGLE, targetPixel, and fovPixel. Finally,
    added calcDistance() method.

--- src/org/usfirst/frc/team948/robot/utilities/VisionProcessing.java
+++ src/org/usfirst/frc/team948/robot/utilities/VisionProcessing.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 public class VisionProcessing {
 	
 	public static NetworkTable table = NetworkTable.getTable("GRIP/myContoursReport");
-	public static NetworkTable tableTwo = NetworkTable.getTable("GRIP/totalAreaReport");
+	public static NetworkTable tableTotalArea = NetworkTable.getTable("GRIP/totalAreaReport");
 	public static double[] totalWidth;
 	public static double[] totalHeight;
 	public static double[] centerX;
@@ -14,6 +14,10 @@ public class VisionProcessing {
 	public static double[] height;
 	public static double[] width;
 	private static final double[] DEFAULT_ARRAY = new double[0];
+	private static final double TARGET_FEET = 2;
+	private static final double FOV_ANGLE = 49.64;
+	private static double targetPixel;
+	private static double fovPixel;
 	
 	public static void updateVision() {
 		centerX = table.getNumberArray("centerX", DEFAULT_ARRAY);
@@ -21,8 +25,8 @@ public class VisionProcessing {
 		area = table.getNumberArray("area", DEFAULT_ARRAY);
 		height = table.getNumberArray("height", DEFAULT_ARRAY);
 		width = table.getNumberArray("width", DEFAULT_ARRAY);
-		totalWidth = tableTwo.getNumberArray("width", DEFAULT_ARRAY);
-		totalHeight = tableTwo.getNumberArray("height", DEFAULT_ARRAY);
+		totalWidth = tableTotalArea.getNumberArray("width", DEFAULT_ARRAY);
+		totalHeight = tableTotalArea.getNumberArray("height", DEFAULT_ARRAY);
 	}
 	
 	public static double getArea() {
@@ -31,6 +35,15 @@ public class VisionProcessing {
 	
 	public static double getWidth() {
 		return width[0];
+	}
+	public static double getTotalWidth(){
+		return totalWidth[0];
+	}
+	public static double calcDistance(){
+		fovPixel = getTotalWidth();
+		targetPixel = getWidth();
+		double distance = TARGET_FEET*fovPixel/(2*targetPixel*Math.tan((FOV_ANGLE/2.0)*Math.PI)/180);
+		return distance;
 	}
 
 }
