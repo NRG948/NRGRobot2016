0f8c884d91721111b68278b1db2b5877e3e201a5
ValliappanValliappan <svalliap@gmail.com>
2016-02-06 23:01:34 +0530

    #76
    Created two methods in the Climber subsystem that take in a parameter
    power and set it to the appropriate motor:
    windWinch()
    extendTapeMeasure()

--- src/org/usfirst/frc/team948/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team948/robot/subsystems/Climber.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team948.robot.subsystems;
 
+import org.usfirst.frc.team948.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Climber extends Subsystem{
@@ -7,7 +9,13 @@ public class Climber extends Subsystem{
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
-		
+		}
+	public void windWinch(double power){
+		RobotMap.climberWinch.set(power);
+	}
+
+	public void extendTapeMeasure(double power){
+		RobotMap.climberTapeMeasure.set(power);;
 	}
 
 }
