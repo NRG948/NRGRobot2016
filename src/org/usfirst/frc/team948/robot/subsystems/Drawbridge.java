82ba82e6425139b53d9aaef6c92eedd5cf5a36bc
Aviral2001 <meaviralgupta@gmail.com>
2016-02-06 09:44:55 -0800

    #75 Created rawRaise method in Drawbridge

--- src/org/usfirst/frc/team948/robot/subsystems/Drawbridge.java
+++ src/org/usfirst/frc/team948/robot/subsystems/Drawbridge.java
@@ -1,13 +1,19 @@
 package org.usfirst.frc.team948.robot.subsystems;
 
+import org.usfirst.frc.team948.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Drawbridge extends Subsystem{
+public class Drawbridge extends Subsystem {
 
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	public void rawRaise(double power) {
+		RobotMap.drawbridgeArm.set(power);
 	}
 
 }
