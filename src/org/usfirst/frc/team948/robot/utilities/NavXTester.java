233e2c06c1192fadfb43e9ce5659b237e390dda5
Jasonlee05077 <jason1998.jl@gmail.com>
2016-02-06 14:58:32 -0800

    NavXTester and PositionTracker3D
    Finished code in position tracker. added things to display. not properly
    working.

--- src/org/usfirst/frc/team948/robot/utilities/NavXTester.java
+++ src/org/usfirst/frc/team948/robot/utilities/NavXTester.java
@@ -7,45 +7,51 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class NavXTester {
-	
+
 	private static AHRS ahrs = RobotMap.ahrs;
-	
+
 	public static void parameterDisplay() {
 
-		//Accelerations
+		// Accelerations
 		SmartDashboard.putNumber("RawAccelX", ahrs.getRawAccelX());
 		SmartDashboard.putNumber("RawAccelY", ahrs.getRawAccelY());
 		SmartDashboard.putNumber("RawAccelZ", ahrs.getRawAccelZ());
-		
-		//Angular velocities
+
+		// Angular velocities
 		SmartDashboard.putNumber("GyroX (deg per s)", ahrs.getRawGyroX());
 		SmartDashboard.putNumber("GyroY (deg per s)", ahrs.getRawGyroY());
 		SmartDashboard.putNumber("GyroZ (deg per s)", ahrs.getRawGyroZ());
-		
-		//Angles
+
+		// Angles
 		SmartDashboard.putNumber("Pitch", ahrs.getPitch());
 		SmartDashboard.putNumber("Yaw", ahrs.getYaw());
-		SmartDashboard.putNumber("Roll", ahrs.getRoll());		
+		SmartDashboard.putNumber("Roll", ahrs.getRoll());
 
-		//Velocities
+		// Velocities
 		SmartDashboard.putNumber("vx (m per s)", ahrs.getVelocityX());
 		SmartDashboard.putNumber("vy (m per s)", ahrs.getVelocityY());
 		SmartDashboard.putNumber("vz (m per s)", ahrs.getVelocityZ());
-		
-		//Positions
+
+		// Positions
 		SmartDashboard.putNumber("dx", ahrs.getDisplacementX());
 		SmartDashboard.putNumber("dy", ahrs.getDisplacementY());
 		SmartDashboard.putNumber("dz", ahrs.getDisplacementZ());
 
-		//Inertial frame accelerations
+		// Inertial frame accelerations
 		SmartDashboard.putNumber("worldax", ahrs.getWorldLinearAccelX());
 		SmartDashboard.putNumber("worlday", ahrs.getWorldLinearAccelY());
 		SmartDashboard.putNumber("worldaz", ahrs.getWorldLinearAccelZ());
-		
-		//Computed world acceleration
-		SmartDashboard.putNumber("computedawx", PositionTracker3D.aworld[0]);
-		SmartDashboard.putNumber("computedawy", PositionTracker3D.aworld[1]);
-		SmartDashboard.putNumber("computedawz", PositionTracker3D.aworld[2]);
-		//SmartDashboard.putString("Yaw axis", ahrs.getBoardYawAxis().toString());
-	}	
+
+		// Computed world acceleration, velocity, and position
+		for (int i = 0; i < 3; i++) {
+			SmartDashboard.putNumber("computeda" + (char) ('x' + i), PositionTracker3D.aworld[i]);
+			SmartDashboard.putNumber("computedv" + (char) ('x' + i), PositionTracker3D.vworld[i]);
+			SmartDashboard.putNumber("computedr" + (char) ('x' + i), PositionTracker3D.rworld[i]);
+			SmartDashboard.putNumber("acal" + (char) ('x' + i), PositionTracker3D.acal[i]);
+			SmartDashboard.putNumber("ocal" + (char) ('x' + i), PositionTracker3D.ocal[i]);
+		}
+
+		SmartDashboard.putString("finalize calibration", PositionTracker3D.finalizeCalibration + "");
+		SmartDashboard.putNumber("valueToCheck", PositionTracker3D.valueToCheck);
+	}
 }
