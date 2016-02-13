42ef22c5b6b2713cc854294d316f3662f2ccb51f
Sai Bulusu <sai.bulusu@gmail.com>
2016-02-12 20:02:37 -0800

    Fixed Code
    Fixed Up LowerChival and LowerDrawbridge code

--- src/org/usfirst/frc/team948/robot/commands/LowerChival.java
+++ src/org/usfirst/frc/team948/robot/commands/LowerChival.java
@@ -1,19 +1,17 @@
 package org.usfirst.frc.team948.robot.commands;
 
+import org.usfirst.frc.team948.robot.Robot;
 import org.usfirst.frc.team948.robot.subsystems.Acquirer;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class LowerChival extends CommandGroup{
 	
-	public static double DEFAULT;
-	public static double PORTCULLIS_LOW;
-	
 	public LowerChival() {
 		//Move acquirer to DEFAULT
-		addSequential(new RaiseAcquirerTo(DEFAULT));
+		addSequential(new RaiseAcquirerTo(Robot.Level.DEFAULT));
 		//Move acquirer to PORTCULLIS_LOW
-		addSequential(new RaiseAcquirerTo(PORTCULLIS_LOW));		
+		addSequential(new RaiseAcquirerTo(Robot.Level.PORTCULLIS_LOW));
 		//Drive forward for 1 foot 
 		addSequential(new DriveStraightFeet(1, 1));//Value needs to be checked
 	}	
