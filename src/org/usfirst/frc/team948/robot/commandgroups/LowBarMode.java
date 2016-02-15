6011ad26d26b42ae880b84e56eb974fa3c0e525f
Sai Bulusu <sai.bulusu@gmail.com>
2016-02-15 14:32:44 -0800

    LowBarMode commandgroup change code
    changed sequential to parallel # 104

--- src/org/usfirst/frc/team948/robot/commandgroups/LowBarMode.java
+++ src/org/usfirst/frc/team948/robot/commandgroups/LowBarMode.java
@@ -9,8 +9,8 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class LowBarMode extends CommandGroup{
 	
 	public LowBarMode() {
-		addSequential(new RaiseAcquirerTo(Robot.Level.PORTCULLIS_LOW));
-		addSequential(new RaiseShooterArmTo(0));
+		addParallel(new RaiseAcquirerTo(Robot.Level.PORTCULLIS_LOW));
+		addParallel(new RaiseShooterArmTo(0));
 		
 		//ALL VALUES NEED TO BE TESTED
 	}
