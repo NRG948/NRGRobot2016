a78850d6ff66300c708a2af4fff2aa0580a06c5d
Tejas2Rangole <tejas.rangole@gmail.com>
2016-02-16 13:08:49 -0800

    changed level in shooter arm to shooter angle

--- src/org/usfirst/frc/team948/robot/commandgroups/TraverseDefenseMode.java
+++ src/org/usfirst/frc/team948/robot/commandgroups/TraverseDefenseMode.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class TraverseDefenseMode extends CommandGroup  {
 
 	public TraverseDefenseMode() {
-		addSequential(new RaiseAcquirerTo(Robot.Level.FULL_BACK_START));
+		addSequential(new RaiseAcquirerTo(Robot.ShooterAngle.FULL_BACK_START));
 		addSequential(new RaiseShooterArmTo(5));//This value might need to be changed later
 	}
 
