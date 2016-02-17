696186f71c05be38f248056eaf970ce132361cde
chandrap2 <chandra2000@outlook.com>
2016-02-17 14:19:07 -0800

    Changed command in ShootSequence
    Changed ShooterRampUp command initialization to RampToRPM(3300)

--- src/org/usfirst/frc/team948/robot/commandgroups/ShootSequence.java
+++ src/org/usfirst/frc/team948/robot/commandgroups/ShootSequence.java
@@ -1,11 +1,10 @@
 package org.usfirst.frc.team948.robot.commandgroups;
 
-import org.usfirst.frc.team948.robot.Robot;
 import org.usfirst.frc.team948.robot.commands.CommandBase;
 import org.usfirst.frc.team948.robot.commands.Interrupt;
 import org.usfirst.frc.team948.robot.commands.RaiseShooterArmTo;
+import org.usfirst.frc.team948.robot.commands.RampToRPM;
 import org.usfirst.frc.team948.robot.commands.Shoot;
-import org.usfirst.frc.team948.robot.commands.ShooterRampUp;
 import org.usfirst.frc.team948.robot.commands.TurnToTarget;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -15,7 +14,7 @@ public class ShootSequence extends CommandGroup {
 	{
 		addSequential(new TurnToTarget());
 		addParallel(new RaiseShooterArmTo());
-		addSequential(new ShooterRampUp());
+		addSequential(new RampToRPM(3300));
 		addSequential(new Shoot());
 		addSequential(new Interrupt(CommandBase.shooterArm));
 	}
