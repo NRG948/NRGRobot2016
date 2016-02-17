e0081bda343cb3784953aaf91a429a54604b13d6
Aviral2001 <meaviralgupta@gmail.com>
2016-02-17 11:59:00 -0800

    Corrected and Updated MoveShooterToLowerLevel

--- src/org/usfirst/frc/team948/robot/commands/MoveShooterToLowerLevel.java
+++ src/org/usfirst/frc/team948/robot/commands/MoveShooterToLowerLevel.java
@@ -1,53 +1,47 @@
 package org.usfirst.frc.team948.robot.commands;
 
-import org.usfirst.frc.team948.robot.subsystems.ShooterArm;
+import org.usfirst.frc.team948.robot.RobotMap;
 import org.usfirst.frc.team948.robot.subsystems.ShooterArm.ShooterAngle;
 
-import edu.wpi.first.wpilibj.command.Command;
+public class MoveShooterToLowerLevel extends CommandBase{
 
-/**
- *
- */
-public class MoveShooterToLowerLevel extends CommandBase {
-	private double angle;
-	private ShooterAngle currentAngle;
+	private ShooterAngle desiredAngle;
 	
+	public MoveShooterToLowerLevel() {
+		// TODO Auto-generated constructor stub
+	requires(shooterArm);
+	}
+	
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		double voltage = RobotMap.shooterLifterEncoder.getVoltage();
+		ShooterAngle currentAngle = shooterArm.findNearestAngle(voltage);	
+		desiredAngle = shooterArm.nextLowerAngle(currentAngle);
+		shooterArm.setDesiredArmAngle(desiredAngle.getValue());
+	}
+	
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		shooterArm.moveArmToDesiredAngle();
+	}
 
-    public MoveShooterToLowerLevel(double angle) {
-    	requires(shooterArm);
-    	this.angle = angle;
-    	
-    	
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	currentAngle = shooterArm.findNearestAngle(angle);
-    	ShooterAngle nextLowerAngle = shooterArm.nextLowerAngle(currentAngle);
-    	shooterArm.setDesiredArmAngle(nextLowerAngle.getValue());
-    }
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		shooterArm.stopArm();
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	
-    	shooterArm.moveArmToDesiredAngle();
-    	
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    	shooterArm.stopArm();
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		end();
+	}
 }
