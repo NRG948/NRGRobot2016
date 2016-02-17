e358f663f33f81d30030084846a277707dc39a48
Tejas2Rangole <tejas.rangole@gmail.com>
2016-02-17 12:22:21 -0800

    Changed name to RaiseShooterToNextLowerLevel

--- src/org/usfirst/frc/team948/robot/commands/RaiseShooterToNextLowerAngle.java
+++ src/org/usfirst/frc/team948/robot/commands/RaiseShooterToNextLowerAngle.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class MoveShooterToLowerLevel extends CommandBase {
+public class RaiseShooterToNextLowerAngle extends CommandBase {
 	private double angle;
 	private ShooterAngle currentAngle;
 	
