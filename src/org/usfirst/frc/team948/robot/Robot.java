b58684b54e36a6ac80342038b772a4c3e5142db7
Sai Bulusu <sai.bulusu@gmail.com>
2016-02-18 15:44:12 -0800

    Robot enum level #122
    Changed volts to angles #122

--- src/org/usfirst/frc/team948/robot/Robot.java
+++ src/org/usfirst/frc/team948/robot/Robot.java
@@ -39,12 +39,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends IterativeRobot {
 	
 	public enum Level {
-		PORTCULLIS_LOW(0.0),
-		DEFAULT(1.0),
-		PORTCULLIS_HIGH(2.0),
-		SALLY_ENGAGED(3.0),
-		SALLY_PORT_HIGH(4.0),
-		FULL_BACK_START(5.0);
+		PORTCULLIS_LOW(0),
+		DEFAULT(36),
+		PORTCULLIS_HIGH(72),
+		SALLY_ENGAGED(108),
+		SALLY_PORT_HIGH(144),
+		FULL_BACK_START(180);
 		
 		private double value;
 
