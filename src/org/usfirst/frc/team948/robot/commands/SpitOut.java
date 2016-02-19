f5d0b4fb0cedab02b09512a28b323acf73d180e6
ValliappanValliappan <svalliap@gmail.com>
2016-02-19 06:01:08 +0530

    #124
    created a command called SpitOut that pushes the ball out after 0.5
    seconds.

---
+++ src/org/usfirst/frc/team948/robot/commands/SpitOut.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team948.robot.commands;
+
+import org.usfirst.frc.team948.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Timer;
+
+public class SpitOut extends CommandBase {
+	Timer halfAsecond = new Timer();
+	private final double TIME = 0.5;
+	private final double POWER;
+
+	public SpitOut() {
+		requires(shooterWheel);
+		requires(shooterBar);
+	}
+
+	@Override
+	protected void initialize() {
+		halfAsecond.reset();
+		halfAsecond.start();
+	}
+
+	@Override
+	protected void execute() {
+		RobotMap.rightShooterWheel.set(POWER);
+		RobotMap.leftShooterWheel.set(-POWER);
+		if (halfAsecond.get() >= TIME) {
+			shooterBar.rawBallPush(POWER);
+		}
+	}
+
+	@Override
+	protected void end() {
+		RobotMap.rightShooterWheel.set(0);
+		RobotMap.leftShooterWheel.set(0);
+		shooterBar.rawBallPush(0);
+		halfAsecond.stop();
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return !shooterWheel.isBallLoaded();
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		end();
+	}
+
+}
