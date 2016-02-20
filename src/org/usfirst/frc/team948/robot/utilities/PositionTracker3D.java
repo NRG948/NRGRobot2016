aad659ebaecf01abb033b0d6c6b5264d68d531b1
Sai Bulusu <sai.bulusu@gmail.com>
2016-02-20 15:39:40 -0800

    Wrote test() in positionTracker3D
    Wrote test() in positionTracker3D

--- src/org/usfirst/frc/team948/robot/utilities/PositionTracker3D.java
+++ src/org/usfirst/frc/team948/robot/utilities/PositionTracker3D.java
@@ -1,5 +1,11 @@
 package org.usfirst.frc.team948.robot.utilities;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import org.usfirst.frc.team948.robot.RobotMap;
 
 import com.kauailabs.navx.frc.AHRS;
@@ -151,4 +157,125 @@ public class PositionTracker3D {
 		ocal[1] += ahrs.getRawGyroY() * dt;
 		ocal[2] += ahrs.getRawGyroZ() * dt;
 	}
+
+//	public static void test() {
+////		Uniform circular motion
+//		double[] araw = {0, -1, 0};
+//		double radius = 10;
+//		double[] gyro = {0,0, Math.sqrt(Math.abs(araw[1] * G) / radius)};
+//		
+//		double dt = 0.02;
+//		double duration = 10;
+//		
+//		vworld = new double[] {gyro[2] * radius, 0, 0};
+//		
+//		final double[] x = new double[1 + (int) (duration/dt)];
+//		final double[] y = new double[1 + (int) (duration/dt)];
+//		final double[] z = new double[1 + (int) (duration/dt)];
+//		
+//		for(int i = 1; i<x.length; i++) {
+//			computePosition(araw, gyro, dt);
+//			x[i] = rworld[0];
+//			y[i] = rworld[1];
+//			z[i] = rworld[2];
+//		}
+//
+//		JFrame frame = new JFrame();
+//		frame.setSize(500, 500);
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		frame.setVisible(true);
+//
+//		frame.add(new JPanel() {
+//			@Override
+//			protected void paintComponent(Graphics g) {
+//				super.paintComponent(g);
+//				// coordinate changes
+//				double radius = 100;
+//				int cx = getWidth() / 2;
+//				int cy = getHeight() / 2;
+//				g.setColor(Color.RED);
+//				for (int i = 0; i < x.length; i++) {
+//					int xp = (int) (cx + x[i] * radius);
+//					int yp = (int) (cy - y[i] * radius);
+//					g.fillOval(xp - 1, yp - 1, 2, 2);
+//				}
+//			}
+//		});
+//
+//	}	
+	
+	private static void computePosition(double[] araw, double[] gyro, double dt) {
+		// Magnitude of the angular velocity
+		double omega = 0;
+		
+		for(double ocomp : gyro) {
+			omega += ocomp * ocomp;
+		}
+
+		omega = Math.sqrt(omega);
+		
+		// Components of the unit vector in angular velocity
+		// Angle of the rotation
+		double angle = omega * dt;
+		if (omega == 0) {
+			omega = 1;
+		}
+		double ux = gyro[0] / omega;
+		double uy = gyro[1] / omega;
+		double uz = gyro[2] / omega;
+		// Rotation matrix
+		// Cosine and sine of the angle of rotation
+		double cos = Math.cos(angle);
+		double sin = Math.sin(angle);
+		// 3-dimensional rotation matrix(see Wikipedia page for three
+		// dimensional rotation matrix)
+		double[][] rot = {
+				{ cos + ux * ux * (1 - cos), ux * uy * (1 - cos) - uz * sin, ux * uz * (1 - cos) + uy * sin },
+				{ ux * uy * (1 - cos) + uz * sin, cos + uy * uy * (1 - cos), uy * uz * (1 - cos) - ux * sin },
+				{ ux * uz * (1 - cos) - uy * sin, uy * uz * (1 - cos) + ux * sin, cos + uz * uz * (1 - cos) } };
+		// New basis vectors of the robot frame size 3 arrays
+		double[] inew = new double[3];
+		double[] jnew = new double[3];
+		double[] knew = new double[3];
+
+		for (int index = 0; index < 3; index++) {
+			inew[index] = rot[0][0] * i[index] + rot[1][0] * j[index] + rot[2][0] * k[index];
+			jnew[index] = rot[0][1] * i[index] + rot[1][1] * j[index] + rot[2][1] * k[index];
+			knew[index] = rot[0][2] * i[index] + rot[1][2] * j[index] + rot[2][2] * k[index];
+		}
+		// Update basis vectors of the robot frame expressed on the fixed frame
+		i = inew;
+		j = jnew;
+		k = knew;
+		// world accelerations(acceleration components in the fixed frame)
+		double[] newAWorld = new double[3];
+		for (int index = 0; index < 3; index++) {
+			newAWorld[index] = araw[0] * i[index] + araw[1] * j[index] + araw[2] * k[index];
+		}
+		// Subtract g since the raw accel measures the true accel minus g
+		newAWorld[2]--;
+		
+		for (int index = 0; index < 3; index++) {
+			newAWorld[index] *= G;
+		}
+		// Switch to navX World Acceleration
+//		newAWorld = new double[] { ahrs.getWorldLinearAccelX() * G, ahrs.getWorldLinearAccelY() * G,
+//				ahrs.getWorldLinearAccelZ() * G };
+
+		double[] newVWorld = new double[3];
+		for (int index = 0; index < 3; index++) {
+			newVWorld[index] = vworld[index] + (newAWorld[index] + aworld[index]) / 2 * dt;
+		}
+		for (int index = 0; index < 3; index++) {
+			rworld[index] += (newVWorld[index] + vworld[index]) / 2 * dt;
+		}
+		// Update velocities and accelerations
+		aworld = newAWorld;
+		vworld = newVWorld;
+		
+	}
+//	
+//	public static void main(String[] args) {
+//		test();
+//	}
 }
