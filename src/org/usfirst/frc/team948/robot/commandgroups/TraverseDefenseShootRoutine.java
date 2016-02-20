99a3c592152edeca035730e29e2b42b5ec749e09
Sai Bulusu <sai.bulusu@gmail.com>
2016-02-20 14:00:29 -0800

    Created a commandgroup
    Created a commandgroup traversedefenseshootroutine

---
+++ src/org/usfirst/frc/team948/robot/commandgroups/TraverseDefenseShootRoutine.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team948.robot.commandgroups;
+
+import org.usfirst.frc.team948.robot.Robot;
+import org.usfirst.frc.team948.robot.Robot.AutoPosition;
+import org.usfirst.frc.team948.robot.commands.DriveStraightDistance;
+import org.usfirst.frc.team948.robot.commands.TurnAngle;
+import org.usfirst.frc.team948.robot.utilities.PositionTracker;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class TraverseDefenseShootRoutine extends CommandGroup {
+	// Positions 1-5(enum Robot.AutoPosition)
+	private static final double AUTO_LINE_TO_OPPONENT_ALIGNMENT_LINE_DISTANCE = 17.22;
+	private static final double TURN_TO_TARGET_POWER = 0.7;
+
+	public TraverseDefenseShootRoutine(Robot.AutoPosition position) {
+		addSequential(new DriveStraightDistance(TURN_TO_TARGET_POWER,
+				AUTO_LINE_TO_OPPONENT_ALIGNMENT_LINE_DISTANCE));
+		addSequential(new TurnAngle(position.getAngle(), TURN_TO_TARGET_POWER));
+		addSequential(new ShootSequence());
+	}
+}
