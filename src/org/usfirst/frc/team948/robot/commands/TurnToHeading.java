62c3fdd8063d686a12fc23913ccdb9102ac40fe5
j-ouyang <jamesouyang00@gmail.com>
2016-03-05 14:20:00 -0800

    Added turnToHeading command

---
+++ src/org/usfirst/frc/team948/robot/commands/TurnToHeading.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team948.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class TurnToHeading extends CommandBase{
+	private static final double DEFAULT_TOLERANCE = 3.0; //This needs value needs to be changed
+	private double finalHeading;
+	private double power;
+	private double tolerance;
+	
+	public TurnToHeading(double heading, double power, double tolerance){
+		requires(drive);
+		this.finalHeading = heading;
+		this.power = power;
+		this.tolerance = tolerance;
+	}
+	
+	public TurnToHeading(double heading, double power){
+		this(heading,power,DEFAULT_TOLERANCE);
+	}
+
+	public TurnToHeading(double power){
+		this(power, 0.5, DEFAULT_TOLERANCE);
+		}
+	
+	
+	protected void initialize (){
+		SmartDashboard.putNumber("turn final heading", finalHeading);
+	}
+	
+	protected void execute(){
+		drive.turnToHeading(finalHeading, power);
+	}
+	
+	protected boolean isFinished(){
+		return drive.turnToHeadingComplete(tolerance); //|| timer.get() > 5;
+	}
+	
+	protected void end(){
+		drive.rawStop();
+		drive.turnToHeadingEnd(finalHeading);  
+	}
+
+	protected void interrupted() {
+		end();
+	}
+}
